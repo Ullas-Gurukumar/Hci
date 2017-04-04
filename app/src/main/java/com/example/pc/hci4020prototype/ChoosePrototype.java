b6cbe6eb8984295e0f5f4c67fa362fa4ff392385
Christopher_Vo <voc@myumanitoba.ca>
2017-04-04 15:02:20 -0500

    check if commit works again

--- app/src/main/java/com/example/pc/hci4020prototype/ChoosePrototype.java
+++ app/src/main/java/com/example/pc/hci4020prototype/ChoosePrototype.java
@@ -9,7 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class ChoosePrototype extends AppCompatActivity {
-//sfs
+//sfss
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
